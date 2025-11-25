d93f65c36e69fcf7bdecee32363234279367e9bc
jospnunes <jospnunes@gmail.com>
2025-11-25 13:31:38 -0300

    feat: Improve BrowserManager to handle tab creation failures and restart browser if necessary

--- src/infra/browser.rs
+++ src/infra/browser.rs
@@ -1,34 +1,65 @@
 use headless_chrome::{Browser, LaunchOptions};
 use anyhow::Result;
-use std::sync::Arc;
+use std::sync::{Arc, RwLock};
 
 #[derive(Clone)]
 pub struct BrowserManager {
-    browser: Arc<Browser>,
+    browser: Arc<RwLock<Browser>>,
 }
 
 impl BrowserManager {
     pub fn new() -> Result<Self> {
+        let browser = Self::create_browser()?;
+        Ok(Self {
+            browser: Arc::new(RwLock::new(browser)),
+        })
+    }
+
+    fn create_browser() -> Result<Browser> {
         let options = LaunchOptions::default_builder()
             .args(vec![
                 std::ffi::OsStr::new("--no-sandbox"),
                 std::ffi::OsStr::new("--disable-gpu"),
                 std::ffi::OsStr::new("--disable-dev-shm-usage"),
+                std::ffi::OsStr::new("--disable-software-rasterizer"),
+                std::ffi::OsStr::new("--disable-extensions"),
             ])
             .build()
             .map_err(|e| anyhow::anyhow!("Failed to build launch options: {}", e))?;
 
-        let browser = Browser::new(options)
-            .map_err(|e| anyhow::anyhow!("Failed to launch browser: {}", e))?;
-
-        Ok(Self {
-            browser: Arc::new(browser),
-        })
+        Browser::new(options)
+            .map_err(|e| anyhow::anyhow!("Failed to launch browser: {}", e))
     }
 
     pub fn print_to_pdf(&self, html: &str) -> Result<Vec<u8>> {
-        let tab = self.browser.new_tab()
-            .map_err(|e| anyhow::anyhow!("Failed to create tab: {}", e))?;
+        // Try to get a tab with current browser
+        let tab = {
+            let browser_guard = self.browser.read().map_err(|_| anyhow::anyhow!("Browser lock poisoned"))?;
+            browser_guard.new_tab()
+        };
+
+        let tab = match tab {
+            Ok(t) => t,
+            Err(e) => {
+                tracing::warn!("Failed to create tab, attempting to restart browser: {}", e);
+                // If failed, try to restart browser
+                let mut browser_guard = self.browser.write().map_err(|_| anyhow::anyhow!("Browser lock poisoned"))?;
+                
+                // Try to create new browser
+                match Self::create_browser() {
+                    Ok(new_browser) => {
+                        *browser_guard = new_browser;
+                        tracing::info!("Browser restarted successfully");
+                        // Try to create tab again
+                        browser_guard.new_tab()
+                            .map_err(|e| anyhow::anyhow!("Failed to create tab after restart: {}", e))?
+                    },
+                    Err(e) => {
+                        return Err(anyhow::anyhow!("Failed to restart browser: {}", e));
+                    }
+                }
+            }
+        };
 
         let html_data_url = format!("data:text/html;charset=utf-8,{}", urlencoding::encode(html));
         
